6f900cfa1cce694d5f10b52e8c090baaf804faeb
ibnuardhians <ibnuardhian@student.gunadarma.ac.id>
2023-10-29 22:28:03 +0700

    edit some

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-  // Fungsi untuk menampilkan notifikasi
   function confirmSubmissionT1() {
-    // Periksa apakah semua bidang yang wajib diisi telah terisi
     const fname = document.querySelector("input[name='fname']").value;
     const notelp = document.querySelector("input[name='notelp']").value;
     const alamat = document.querySelector("textarea[name='alamat']").value;
@@ -25,18 +23,13 @@
       alert("Silahkan lengkapi semua data terlebih dahulu!");
     } else {
       if (confirm("Apakah yakin data sudah benar?")) {
-        // Jika pengguna menekan OK, maka form akan disubmit
         document.querySelector("form").submit();
-        
-        // Tampilkan notifikasi "Terima Kasih sudah memesan :)"
         alert("Terima Kasih sudah memesan :)");
       }
     }
   }
 
-// Fungsi untuk menampilkan notifikasi untuk t3
 function confirmSubmissionT3() {
-  // Periksa apakah semua bidang yang wajib diisi untuk t3 telah terisi
   const username = document.querySelector("input[name='username']").value;
   const pass = document.querySelector("input[name='pass']").value;
   const email = document.querySelector("input[name='email']").value;
@@ -46,24 +39,17 @@ function confirmSubmissionT3() {
     alert("Silahkan lengkapi semua data terlebih dahulu!");
   } else {
     if (confirm("Apakah yakin data sudah benar?")) {
-      // Jika pengguna menekan OK, maka form t3 akan disubmit
       document.querySelector("form[name='formulir-member']").submit();
-
-      // Tampilkan notifikasi "Data member tersimpan :)"
       alert("Data member tersimpan :)");
     }
   }
 }
-
-// Menambahkan event listener pada tombol "Simpan Pesanan" (t1)
 document.querySelector("input[name='t1']").addEventListener("click", function (e) {
-  e.preventDefault(); // Mencegah pengiriman form otomatis
+  e.preventDefault();
   confirmSubmissionT1();
 });
-
-// Menambahkan event listener pada tombol "Simpan" (t3)
 document.querySelector("input[name='t3']").addEventListener("click", function (e) {
-  e.preventDefault(); // Mencegah pengiriman form otomatis
+  e.preventDefault();
   confirmSubmissionT3();
 });
 
@@ -77,13 +63,12 @@ document.querySelector("input[name='t3']").addEventListener("click", function (e
   
     reader.onload = function () {
       preview.src = reader.result;
-      dropspan.style.display = 'none'; // Menghilangkan span ketika gambar diunggah
+      dropspan.style.display = 'none';
     };
-  
     if (file) {
       reader.readAsDataURL(file);
     } else {
       preview.src = '';
-      dropspan.style.display = 'block'; // Menampilkan span jika tidak ada gambar yang dipilih
+      dropspan.style.display = 'block';
     }
   });
